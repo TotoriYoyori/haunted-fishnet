cd9e6fed97eaa84b51aa4749becc490d8013ec24
TheGiraffeInTheBathtub <franciszek.ryt.9227@student.uu.se>
2025-02-05 15:50:27 +0100

    scripts for tutorial elements
    scripts to keep track of tutorial part

---
+++ Assets/Tutorial/RoomCheckScript.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class RoomCheckScript : MonoBehaviour
+{
+    public bool tutorial_part_1;
+    public bool tutorial_part_2;
+    public bool tutorial_part_3;
+    public bool tutorial_part_4;
+    public bool tutorial_part_5;
+    public bool tutorial_part_6;
+    public bool tutorial_part_7;
+
+    bool kill = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Deinitialise();
+    }
+
+    void Deinitialise()
+    {
+        if (tutorial_part_1 && TutorialProgress.part == 2) { kill = true; }
+        else if (tutorial_part_2 && TutorialProgress.part == 3) { kill = true; }
+        else if (tutorial_part_3 && TutorialProgress.part == 4) { kill = true; }
+        else if (tutorial_part_4 && TutorialProgress.part == 5) { kill = true; }
+        else if (tutorial_part_5 && TutorialProgress.part == 6) { kill = true; }
+        else if (tutorial_part_6 && TutorialProgress.part == 7) { kill = true; }
+
+
+        if (kill) { gameObject.SetActive(false); }
+    }
+
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.CompareTag("Player"))
+        {
+            if (tutorial_part_1 && TutorialProgress.part == 1) { TutorialProgress.part = 2; }
+            else if (tutorial_part_2 && TutorialProgress.part == 2) { TutorialProgress.part = 3; }
+            else if (tutorial_part_3 && TutorialProgress.part == 3) { TutorialProgress.part = 4; }
+            else if (tutorial_part_4 && TutorialProgress.part == 4) { TutorialProgress.part = 5; }
+            else if (tutorial_part_5 && TutorialProgress.part == 5) { TutorialProgress.part = 6; }
+            else if (tutorial_part_6 && TutorialProgress.part == 6) { TutorialProgress.part = 7; }
+        }
+    }
+
+}
