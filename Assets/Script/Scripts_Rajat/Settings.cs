31021c568e71cfbb7626c3a8f796a40d5697de6b
X77O <x770gaming@gmail.com>
2025-02-19 12:00:38 +0100

    Pausemenu and setting menu
    The pause menu and settings menu is on the PauseTempScene, it can be imported into the game. When putting in main game, replace the old pause menu on lvl_tilemap, and hook it up to use inputcontroller instead of pressing escape.

--- Assets/Script/Scripts_Rajat/Settings.cs
+++ Assets/Script/Scripts_Rajat/Settings.cs
@@ -6,30 +6,51 @@ using UnityEngine.UI;
 public class Settings : MonoBehaviour
 {
     public Toggle fullScreenToggle;
-    public Toggle vSyncToggle;
     public TMP_Dropdown resolutionDropdown;
     public TMP_Dropdown qualityDropdown;
     public TextMeshProUGUI fpsText;
 
-    private Resolution[] resolutions;
+    private List<Resolution> filteredResolutions;
     private float deltaTime = 0.0f;
 
     void Start()
     {
         // Load available screen resolutions
-        resolutions = Screen.resolutions;
+        Resolution[] allResolutions = Screen.resolutions;
         resolutionDropdown.ClearOptions();
+
+        List<Resolution> resolutions16_9 = new List<Resolution>();
+
+        foreach (Resolution res in allResolutions)
+        {
+            float aspectRatio = (float)res.width / res.height;
+            if (Mathf.Approximately(aspectRatio, 16f / 9f))
+            {
+                resolutions16_9.Add(res);
+            }
+        }
+
+        // Sort resolutions by width and height
+        resolutions16_9.Sort((a, b) => (b.width * b.height).CompareTo(a.width * a.height));
+
+        // Supposed to limit to the top 10 resolutions for 16:9, It limits to 3 on my laptop for some reason
+        if (resolutions16_9.Count > 10)
+        {
+            resolutions16_9 = resolutions16_9.GetRange(0, 10);
+        }
+
+        filteredResolutions = resolutions16_9;
 
         List<string> resolutionOptions = new List<string>();
         int currentResolutionIndex = 0;
 
-        for (int i = 0; i < resolutions.Length; i++)
+        for (int i = 0; i < filteredResolutions.Count; i++)
         {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
+            string option = filteredResolutions[i].width + " x " + filteredResolutions[i].height;
             resolutionOptions.Add(option);
 
-            if (resolutions[i].width == Screen.currentResolution.width &&
-                resolutions[i].height == Screen.currentResolution.height)
+            if (filteredResolutions[i].width == Screen.currentResolution.width &&
+                filteredResolutions[i].height == Screen.currentResolution.height)
             {
                 currentResolutionIndex = i;
             }
@@ -47,20 +68,18 @@ public class Settings : MonoBehaviour
         qualityDropdown.value = QualitySettings.GetQualityLevel();
         qualityDropdown.RefreshShownValue();
 
-        // Initialize fullscreen and V-Sync toggle states
+        // Fullscreen
         fullScreenToggle.isOn = Screen.fullScreen;
-        vSyncToggle.isOn = QualitySettings.vSyncCount > 0; // If vSyncCount = 0, V-Sync is disabled
 
-        // Add listeners
+        // Listeners
         resolutionDropdown.onValueChanged.AddListener(SetResolution);
         fullScreenToggle.onValueChanged.AddListener(SetFullScreen);
         qualityDropdown.onValueChanged.AddListener(SetQuality);
-        vSyncToggle.onValueChanged.AddListener(SetVSync);
     }
 
     public void SetResolution(int index)
     {
-        Resolution resolution = resolutions[index];
+        Resolution resolution = filteredResolutions[index];
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
@@ -72,12 +91,6 @@ public class Settings : MonoBehaviour
     public void SetQuality(int index)
     {
         QualitySettings.SetQualityLevel(index);
-    }
-
-    public void SetVSync(bool isEnabled)
-    {
-        // Enable V-Sync (1)
-        QualitySettings.vSyncCount = isEnabled ? 1 : 0;
     }
 
     void Update()
